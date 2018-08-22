a090b100857f2f78fbe2c4191bd1aa072a54cab7
Ruchi <ruchi.kumari4@cognizant.com>
2018-08-22 06:31:19 +0000

    Change LoginComponent

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,8 +1,9 @@
-import { Component, ViewChild } from '@angular/core';
-import { FormControl, FormGroup, FormGroupDirective } from '@angular/forms';
+import { Component } from '@angular/core';
+import { Validators } from '@angular/forms';
+import { FormControl } from '@angular/forms';
 import { AuthenticationService } from '../services/authentication.service';
 import { RouterService } from '../services/router.service';
-
+import { Register } from '../register';
 
 
 @Component({
@@ -10,42 +11,36 @@ import { RouterService } from '../services/router.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
+
 export class LoginComponent {
-  public username;
-  public password;
-  public submitMessage: string;
-  public loginForm: FormGroup = new FormGroup({
-    username: new FormControl(),
-    password: new FormControl()
-  });
+  submitMessage: any;
+  username = new FormControl('', [Validators.required]);
+  password = new FormControl('', [Validators.required]);
 
-  @ViewChild(FormGroupDirective)
-  formGroupDirective: FormGroupDirective;
-
-  constructor(private authService: AuthenticationService,
-    private routerService: RouterService) {
-
-  }
-
+  constructor(private authService: AuthenticationService, private routerService: RouterService) {}
 
   loginSubmit() {
-    console.log(this.loginForm.value);
-    this.authService.authenticateUser(this.loginForm.value).subscribe(response => {
-      console.log('response', response);
-      this.authService.setBearerToken(response['token']);
-      this.routerService.routeToDashboard();
-    },
-      error => {
-        if (error.status === 403) {
-          this.submitMessage = error.error.message;
-        } else {
-          this.submitMessage = error.message;
+    const loginData = new Register(this.username.value, this.password.value);
+    if (this.username.valid && this.password.valid) {
+      this.authService.authenticateUser(loginData).subscribe(
+        response => {
+          // Set the token in localstorage
+          this.authService.setBearerToken(response['token']);
+          this.routerService.routeToDashboard();
+        },
+        err => {
+          // get the error message based on the status
+          this.submitMessage = (err.status === 403) ? err.error.message : err.message ;
         }
-
-        console.log('err', error);
-      });
-    this.loginForm.reset();
-    this.formGroupDirective.resetForm();
-
+      );
+    }
+  }
+  getUserNameErrorMessage() {
+    // Required error message for username
+    return this.username.hasError('required') ? 'You must enter a Username' : '';
+  }
+  getPasswordErrorMessage() {
+    // Required error message for password
+    return this.password.hasError('required') ? 'You must enter a password' : '';
   }
 }
---
+++ src/app/register.ts
@@ -0,0 +1,9 @@
+export class Register {
+    username: String;
+    password: String;
+      constructor(username, password) {
+      this.username = username;
+      this.password = password;
+    }
+  }
+
